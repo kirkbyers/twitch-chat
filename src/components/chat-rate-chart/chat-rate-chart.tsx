310d3dda133afc142558f15c0fce6b942f2fc17a
kirkbyers <kirklbyers@gmail.com>
2019-09-04 22:21:04 -0500

    Add chat-rate-chart label.

--- src/components/chat-rate-chart/chat-rate-chart.tsx
+++ src/components/chat-rate-chart/chat-rate-chart.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-
 import { LineChart, Line, ResponsiveContainer, YAxis } from 'recharts';
+import { Row, Col, Typography } from 'antd';
+
+import './chat-rate-chart.scss';
+
+const { Title } = Typography;
 
 interface Props {
     data: number[]
@@ -9,12 +13,19 @@ interface Props {
 const ChatRateChart: React.SFC<Props> = ({ data = [] }) => {
     const formatedData = data.map((value, index) => ({ value, index }));
     return (
-        <ResponsiveContainer width="100%" height={100}>
-            <LineChart data={formatedData}>
-                <YAxis></YAxis>
-                <Line type="monotone" dataKey="value" stroke="#8884d8" strokeWidth={2} isAnimationActive={false} />
-            </LineChart>
-        </ResponsiveContainer>
+        <Row type="flex" className="chat-rate-chart">
+            <Col>
+                <Title level={3}>Messages Per Second</Title>
+            </Col>
+            <Col>
+                <ResponsiveContainer width="100%" height={100}>
+                    <LineChart data={formatedData}>
+                        <YAxis></YAxis>
+                        <Line type="monotone" dataKey="value" stroke="#8884d8" strokeWidth={2} isAnimationActive={false} />
+                    </LineChart>
+                </ResponsiveContainer>
+            </Col>
+        </Row>
     );
 }
 
